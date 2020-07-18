6488686ecc5bf7b0df5cc0b38d2a9a093d1e208f
clarisson28 <reddieson05@gmail.com>
2020-07-18 13:49:14 +0800

    If Statement

--- Program.cs
+++ Program.cs
@@ -6,8 +6,36 @@ namespace myApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("The current time is " + DateTime.Now);
+            Console.WriteLine("Ed's Big Give Away!");
+            Console.Write("Choose a door: 1, 2, 3: ");
+            string userValue = Console.ReadLine();
+
+            if (userValue == "1")
+            {
+                string message = "You won a new car!";
+                Console.WriteLine(message);
+            }
+
+            else if (userValue == "2")
+            {
+                string message = "You won a new boat!";
+                Console.WriteLine(message);
+            }
+
+            else if (userValue == "3")
+            {
+                string message = "You won a new cat!";
+                Console.WriteLine(message);
+            }
+
+            else
+            {
+                string message = "Sorry, we didn't understand.";
+                Console.WriteLine(message);
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
