8352114d78a0fb8ec76878a4bc2f4447429ba7df
clarisson28 <reddieson05@gmail.com>
2020-07-18 16:49:52 +0800

    Understanding Data Types and Variables

--- Program.cs
+++ Program.cs
@@ -6,43 +6,27 @@ namespace myApp
     {
         static void Main(string[] args)
         {
-            /*Console.WriteLine("Ed's Big Give Away!");
-            Console.Write("Choose a door: 1, 2, 3: ");
-            string userValue = Console.ReadLine();
-
-            string message = "";
-            if (userValue == "1")
-                message = "You won a new car!";
-            else if (userValue == "2")
-                message = "You won a new boat!";
-            else if (userValue == "3")
-                message = "You won a new cat!";
-            else
-            {
-                message = "Sorry, we didn't understand. ";
-                message += "You Lose.";
-            }
-
-            Console.WriteLine(message);
-            Console.ReadLine();*/
-
-
-            Console.WriteLine("Ed's Big Give Away!");
-            Console.Write("Choose a door: 1, 2, 3: ");
-            string userValue = Console.ReadLine();
-
-            string message = (userValue == "1") ? "boat" : "strand of lint";
+            /*int x, y;
+            x = 7;
+            y = x + 3;
             
-            //Console.Write("You won a ");
-            //Console.Write(message);
-            //Console.Write(".");
-            //Console.ReadLine();
+            Console.WriteLine(y);*/
 
-            //Console.WriteLine("You won a {0}.", message);
-            Console.WriteLine("You entered: {0}, therefore you won a {1}.", userValue, message);
+            Console.WriteLine("What is your name?");
+            Console.Write("Type your first name: ");
+            string myFirstName;
+            myFirstName = Console.ReadLine();
+
+            //string myLastName;
+            //Console.Write("Type your last name: ");
+            //myLastName = Console.ReadLine();
+
+            Console.Write("Type your last name: ");
+            string myLastName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + myFirstName + " " + myLastName);
 
             Console.ReadLine();
-
         }
     }
 }
